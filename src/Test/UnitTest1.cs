cf402d98133cf2106eb5150913d0d9cc34674746
Mikiya Mizui <mikiya.mizui@gmail.com>
2019-08-03 17:39:51 +0900

    changed UnitTest

--- src/Test/UnitTest1.cs
+++ src/Test/UnitTest1.cs
@@ -1,8 +1,9 @@
+using Dapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Npgsql;
 using Serilog;
 using System;
-using System.Linq;
+using System.Threading.Tasks;
 using Zengo;
 using Zengo.CsvFile;
 using Zengo.TextFile;
@@ -19,70 +20,11 @@ namespace Test
                 .WriteTo.File("sql.log")
                 .CreateLogger();
 
-            SqlTools.SqlLog = (sql, parameters) => logger.Information("{Sql} [{Parameters}]", sql, string.Join(", ", parameters.Select(p => $"{p.Name}:\"{p.Value}\"")));
-            SqlTools.SelectLog = (sql, keys) => logger.Information("{Sql} {@Keys}", sql, keys);
-            SqlTools.InsertLog = (sql, keys) => logger.Information("{Sql} {@Keys}", sql, keys);
-            SqlTools.UpdateLog = (sql, updates, keys) => logger.Information("{Sql} {@Updates} {@Keys}", sql, updates, keys);
-            SqlTools.DropLog = (sql, keys) => logger.Information("{Sql} {@Keys}", sql, keys);
-
             using (var connection = new NpgsqlConnection("Server=localhost;Port=5432;Database=test;UserId=postgres;Password=postgres;Enlist=true;"))
             {
                 connection.Open();
 
-                connection.ExecuteNonQuery(null, ddl);
-
-                connection.ExecuteInsert(null, table,
-                    new
-                    {
-                        product_name = "Apple",
-                        product_price = 100,
-                        supplier_id = 1,
-                        created_by = "John",
-                        updated_by = "John"
-                    });
-
-                connection.ExecuteInsert(null, table,
-                    new
-                    {
-                        product_name = "Orange",
-                        product_price = 150,
-                        supplier_id = 2,
-                        created_by = "John",
-                        updated_by = "John"
-                    });
-
-                connection.ExecuteInsert(null, table,
-                    new
-                    {
-                        product_name = "Melon",
-                        product_price = 200,
-                        supplier_id = 3,
-                        created_by = "John",
-                        updated_by = "John"
-                    });
-
-                var zl = new ConfigManager<NpgsqlDataAdapter>(connection);
-
-                using (zl.Table(table, "order by product_id").AsTextFile().AsCsvFile())
-                {
-                    connection.ExecuteUpdate(null, table,
-                        new
-                        {
-                            product_price = 165,
-                            updated_at = DateTime.Now,
-                            updated_by = "Sam"
-                        },
-                        new
-                        {
-                            product_id = 1
-                        });
-                }
-            }
-        }
-
-        private const string table = "zengo_test";
-
-        private readonly string ddl = $@"
+                connection.Execute($@"
 drop table if exists zengo_test;
 create table {table}
 (
@@ -96,6 +38,28 @@ create table {table}
     updated_by character varying(20) default null,
     constraint products_pkey primary key (product_id)
 );
-";
+");
+
+                connection.Execute($@"insert into {table} (product_name, product_price, supplier_id, created_by, updated_by) values (@product_name, @product_price, @supplier_id, @created_by, @updated_by)",
+                    new[]
+                    {
+                        new { product_name = "Apple", product_price = 100, supplier_id = 1, created_by = "John", updated_by = "John" },
+                        new { product_name = "Orange", product_price = 150, supplier_id = 2, created_by = "John", updated_by = "John" },
+                        new { product_name = "Melon", product_price = 200, supplier_id = 3, created_by = "John", updated_by = "John" }
+                    });
+
+                var zl = new ConfigManager<NpgsqlDataAdapter>(connection);
+
+                using (zl.Table(table, "order by product_id").AsTextFile().AsCsvFile())
+                {
+                    Task.Delay(TimeSpan.FromSeconds(3)).Wait();
+
+                    connection.Execute($@"update {table} set product_price = @product_price, updated_at = now(), updated_by = @updated_by where product_id = @product_id",
+                        new { product_id = 1, product_price = 165, updated_by = "Sam" });
+                }
+            }
+        }
+
+        private const string table = "zengo_test";
     }
 }
